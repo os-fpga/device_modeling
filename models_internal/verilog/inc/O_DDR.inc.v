adaefe7ec9c63348e122fbe0521cb8cac3baa566
muhammadhamza15 <muhammadhamza1507@gmail.com>
2024-06-12 12:28:26 +0500

    Fixing O_DDR primitive EDA-2950 fixes

--- models_internal/verilog/inc/O_DDR.inc.v
+++ models_internal/verilog/inc/O_DDR.inc.v
@@ -1,12 +1,40 @@
 
+  reg Q0;
+  reg Qp;
+  reg Q1;
+
   always @(negedge R)
     Q <= 1'b0;
 
-  always @(C)
+  always@(posedge C)
+  begin
+    if(!R)
+    begin
+      Qp<=0;
+      Q0<=0;
+    end
+
+    else 
+    begin
+      Q0<=D[0];
+      Qp<=D[1];
+    end
+  end
+
+  always@(negedge C)
+  begin
+    if(!R)
+      Q1<=0;
+    else
+      Q1<=Qp;
+  end
+
+  
+  always @(*)
     if (!R)
       Q <= 1'b0;
     else if (E) 
       if (C)
-        Q <= D[0];
+        Q <= Q0;
       else
-        Q <= D[1];
+        Q <= Q1;
