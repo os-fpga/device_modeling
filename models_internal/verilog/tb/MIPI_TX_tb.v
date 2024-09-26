7d07c5fadd83ec4529ad56adf53f66b267cedad2
muhammadhamza15 <muhammadhamza1507@gmail.com>
2024-09-26 15:45:31 +0500

    adding updated tb for MIPI

--- models_internal/verilog/tb/MIPI_TX_tb.v
+++ models_internal/verilog/tb/MIPI_TX_tb.v
@@ -28,6 +28,13 @@ module MIPI_TX_tb;
   reg  CHANNEL_BOND_SYNC_IN;
   wire  CHANNEL_BOND_SYNC_OUT;
 
+  reg [WIDTH-1:0] HS_TX_DATA1=0;
+  reg [WIDTH-1:0] HS_TX_DATA2=0;
+  reg [WIDTH-1:0] HS_TX_DATA3=0;
+  reg [WIDTH-1:0] comp_data=0;
+  integer error=0;
+
+
   MIPI_TX # (
     .WIDTH(WIDTH),
     .EN_ODLY(EN_ODLY),
@@ -82,16 +89,54 @@ module MIPI_TX_tb;
 		PLL_LOCK=1;
 		repeat(260)@(posedge RX_CLK);
 		HS_TX_DATA=4'b0101;
+    HS_TX_DATA1=HS_TX_DATA;
 		@(negedge CLK_IN);
 		HS_TX_DATA=$urandom();
+    HS_TX_DATA2=HS_TX_DATA;
 		@(negedge CLK_IN);
 		HS_TX_DATA=$urandom();
-		#1000;
+    HS_TX_DATA3=HS_TX_DATA;
+    @(negedge MIPI_TX_inst.O_SERDES_inst.word_load_en_sync);
+    repeat(4)@(RX_CLK)
+    begin
+      comp_data={comp_data[WIDTH-2:0],TX_DP};
+    end
+    if(comp_data!==HS_TX_DATA1)
+      error=error+1;
+
+    @(negedge MIPI_TX_inst.O_SERDES_inst.word_load_en_sync);
+    repeat(4)@(RX_CLK)
+    begin
+      comp_data={comp_data[WIDTH-2:0],TX_DP};
+    end
+    if(comp_data!==HS_TX_DATA2)
+      error=error+1;
+
+    @(negedge MIPI_TX_inst.O_SERDES_inst.word_load_en_sync);
+    repeat(4)@(RX_CLK)
+    begin
+      comp_data={comp_data[WIDTH-2:0],TX_DP};
+    end
+    if(comp_data!==HS_TX_DATA3)
+      error=error+1;
+
+    #1000;
     HS_EN=0;
     LP_EN=1;
-    @(negedge CLK_IN);
-		TX_LP_DN=$urandom();
-		TX_LP_DP=$urandom();
+    repeat(50)@(negedge CLK_IN)
+    begin
+      TX_LP_DP=$urandom();
+		  TX_LP_DN=~TX_LP_DP;
+      #1;
+      if(TX_LP_DP!==TX_DP || TX_LP_DN!==TX_DN)
+        error=error+1;
+    end
+    #2;
+    if(error===0)
+      $display("Test Passed");
+    else
+      $display("Test Failed");
+
     #1000;
     TX_ODT_EN=1;
     #100;
